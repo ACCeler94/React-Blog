7a13b29e6c8493ea5ce84322904cd9a851262179
Artur C <araghast94@icloud.com>
2023-07-20 12:52:20 +0200

    Add responsive NavBar

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -5,13 +5,16 @@ import { Link, NavLink } from "react-router-dom";
 const NavBar = () => {
 
   return (
-    <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+    <Navbar collapseOnSelect bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
       <Container>
         <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
-        <Nav>
-          <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-lg-end">
+          <Nav>
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
