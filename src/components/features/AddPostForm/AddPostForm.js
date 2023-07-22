5fc578f843c70018b866ced00d4ea97536974ccf
Artur C <araghast94@icloud.com>
2023-07-22 19:37:13 +0200

    Add logic to add posts to state

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -1,20 +1,58 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addPost } from "../../../redux/postsRedux";
+import { nanoid } from "nanoid";
+import { useNavigate } from "react-router-dom";
+
 const AddPostForm = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishedDate, setPublishedDate] = useState('');
+  const [shortDescription, setShortDescription] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addPost({
+      id: nanoid(),
+      title,
+      author,
+      publishedDate,
+      shortDescription,
+      content
+    }));
+    setTitle('');
+    setAuthor('');
+    setPublishedDate('');
+    setShortDescription('');
+    setContent('');
+    navigate('/')
+  }
 
   return (
     <div className="post_add">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group short-inputs col-lg-6">
-          <label for="title"  >Title</label><br />
-          <input type="text" id="title" name="title" placeholder="Enter title" className="form-control" required /><br />
-          <label for="author" >Author</label><br />
-          <input type="name" id="author" name="author" placeholder="Enter author" className="form-control" required /> <br />
-          <label for="published-date" >Published</label><br />
-          <input type="date" id="published-date" name="published-date" placeholder="dd-mm-yyyy" className="form-control" required /><br />
+          <label for="title" className="mb-1"  >Title</label><br />
+          <input type="text" id="title" name="title" placeholder="Enter title" className="form-control" required onChange={e => setTitle(e.target.value)} />
+          <br />
+          <label for="author" className="mb-1" >Author</label><br />
+          <input type="name" id="author" name="author" placeholder="Enter author" className="form-control" required onChange={e => setAuthor(e.target.value)} />
+          <br />
+          <label for="published-date" className="mb-1" >Published</label><br />
+          <input type="date" id="published-date" name="published-date" placeholder="dd-mm-yyyy" className="form-control" required onChange={e => setPublishedDate(e.target.value)} />
+          <br />
         </div>
-        <label for="description" >Short description</label><br />
-        <textarea id="description" name="description" placeholder="Enter short description" className="form-control" required ></textarea><br />
-        <label for="content" >Main content</label><br />
-        <textarea id="content" name="content" placeholder="Enter the main content of the post" className="form-control" style={{ height: "200px" }} required></textarea><br />
+        <label for="description" className="mb-1" >Short description</label><br />
+        <textarea id="description" name="description" placeholder="Enter short description" className="form-control" required onChange={e => setShortDescription(e.target.value)}></textarea>
+        <br />
+        <label for="content" className="mb-1" >Main content</label><br />
+        <textarea id="content" name="content" placeholder="Enter the main content of the post" className="form-control" style={{ height: "200px" }} required onChange={e => setContent(e.target.value)}></textarea>
+        <br />
         <button type="submit" className="btn btn-primary">Add post</button>
       </form>
     </div>
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -5,15 +5,19 @@ export const getPostById = ({ posts }, id) => posts.find(post => post.id === id)
 
 const createActionName = actionName => `app/posts/${actionName}`;
 const REMOVE_POST = createActionName('REMOVE_POST');
+const ADD_POST = createActionName('ADD_POST');
 
 // actions
-export const removePost = payload => ({ type: REMOVE_POST, payload })
+export const removePost = payload => ({ type: REMOVE_POST, payload });
+export const addPost = payload => ({ type: ADD_POST, payload });
 
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
     case REMOVE_POST:
       return statePart.filter(post => post.id !== action.payload)
+    case ADD_POST:
+      return [...statePart, action.payload]
     default:
       return statePart;
   };
